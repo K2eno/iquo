7ca28b277b6dd647742b31445ec3b8ed5bc5e45b
Alexander <37315173+K2eno@users.noreply.github.com>
2024-02-16 01:38:28 +0300

    Update Config.sol

--- contracts/Config.sol
+++ contracts/Config.sol
@@ -2,67 +2,47 @@
 
 pragma solidity ^0.8.18;
 
-import "./interfaces/IDeploy.sol";
+import "./interfaces/IConfig.sol";
 import "./interfaces/IErrors.sol";
 
 /** 
- * @title Deploy contract
- * @dev Manages all contract addresses for a currency
+ * @title Config contract
+ * @dev Stores global parameters
  */
-contract Deploy is IDeploy, IErrors {
+contract Config is IConfig, IErrors {
 
-    address public superAdmin;
-    address public config;
+    // VARIABLES
 
-    struct Addresses {
-        address rounds;
-        address signals;
-        address quotes;
-    }
+    address public globalAdmin;
+    uint public  baseTicketPrice = 1_000_000 gwei; // minimal ticket price in native BC currency, measured in gWei: 0.0005 MATIC
 
-    mapping (string => Addresses) contracts; // [currency] => {addresses}
+    // CONSTANTS
 
-    modifier superAdminOnly() {
-        if (msg.sender != superAdmin) {
-            revert CallerIsNotSuperAdmin();
+    uint64 public constant MODEL_HORIZON = 28 * 24 * 60 * 60; // max predictions duration, in seconds: 28 days
+    uint64 public constant HALT_TIME_SPAN = 2 * 60 * 60; // time period immediately preceding snapshot time, in seconds: 2 hours
+    uint8 public constant MAX_NUMBER_OF_ACTIVE_ROUNDS = 4;
+    uint16 public constant MAX_SERVICE_PERCENT = 2000; // max applicable service fee percent, in percent points: 20.00%
+    bool public constant TEST_MODE = true;
+
+    // MODIFIERS
+
+    modifier globalAdminOnly() {
+        if (msg.sender != globalAdmin) {
+            revert CallerIsNotGlobalAdmin();
         }
         _;
     }
-
-    constructor() {
-        superAdmin = msg.sender; // contract creator is super admin
+    
+    constructor() payable {
+        globalAdmin = msg.sender; // contract creator is global admin
     }
 
-    function setSuperAdmin(address _superAdmin) external superAdminOnly() {
-        superAdmin = _superAdmin;
+    function setGlobalAdmin(address _globalAdmin) external globalAdminOnly() {
+        globalAdmin = _globalAdmin;
     }
 
-    function setConfig(address _config) external superAdminOnly() {
-        config = _config;
-    }
-
-    function setRounds(string memory _currency, address _rounds) external superAdminOnly() {
-        contracts[_currency].rounds = _rounds;
-    }
-
-    function setSignals(string memory _currency, address _signals) external superAdminOnly() {
-        contracts[_currency].signals = _signals;
-    }
-
-    function setQuotes(string memory _currency, address _quotes) external superAdminOnly() {
-        contracts[_currency].quotes = _quotes;
-    }
-
-    function getRounds(string memory _currency) external view returns (address) {
-        return contracts[_currency].rounds;
-    }
-
-    function getSignals(string memory _currency) external view returns (address) {
-        return contracts[_currency].signals;
-    }
-
-    function getQuotes(string memory _currency) external view returns (address) {
-        return contracts[_currency].quotes;
+    function setBaseTicketPrice(uint _baseTicketPrice) external globalAdminOnly() {
+        baseTicketPrice = _baseTicketPrice;
     }
 
 }
